d38245808d798a7cab9b8314178b442c1a4004c1
Ricardo Fernandes de Aquino <Ricardo.Fernandes02082004@gmail.com>
2024-06-06 23:32:56 -0300

    Create DataEntry.ino

---
+++ DataEntry/DataEntry.ino
@@ -0,0 +1,10 @@
+int potPin = A0;  
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);
+  Serial.println(potValue);
+  delay(100);                         
+}
